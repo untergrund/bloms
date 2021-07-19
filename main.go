df9a566c6ba3a582e678b8c2cc40805aee82b7e3
Your Name <you@example.com>
2021-07-20 01:05:05 +0200

    Add URL mux

--- main.go
+++ main.go
@@ -5,29 +5,49 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
-    "os"
 )
 
 type response struct {
 	Message string `json:"message"`
 }
 
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	currentTime := time.Now()
+	respJSON, _ := json.Marshal(currentTime)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(respJSON)
+}
+
+func Greet(w http.ResponseWriter, r *http.Request) {
+	response := "Hello from Bloms!"
+	respJSON, _ := json.Marshal(response)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(respJSON)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		response := "Hello fron Bloms!"
-		respJSON, _ := json.Marshal(response)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(respJSON)
-	})
-	port := ":" + os.Getenv("PORT")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", Greet)
+	mux.HandleFunc("/health", HealthCheck)
+
+	port := ""
+	if os.Getenv("PORT") != "" {
+		port = ":" + os.Getenv("PORT")
+	} else {
+		port = ":8000"
+	}
+
 	server := &http.Server{
+		Handler:        mux,
 		Addr:           port,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
+
 	fmt.Printf("Starting server on port %s\n", port)
 	log.Fatal(server.ListenAndServe())
-
 }
